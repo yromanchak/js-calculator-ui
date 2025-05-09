30ce7540d6a8c350d55e3b7766f17496bb577682
yromanchak <yurij.romanchak@gmail.com>
2025-05-09 16:12:30 +0300

    try amend 9

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
-import './assets/styles/main.scss';
-import { Calculator } from './components/calculator.js';
+import "./assets/styles/main.scss";
+import { Calculator } from "./components/calculator.js";
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   const calculator = new Calculator();
-
-  document.querySelectorAll('[data-number]').forEach(button =>
-    button.addEventListener('click', () => calculator.addNumber(button.innerText))
-  );
-  document.querySelectorAll('[data-operation]').forEach(button =>
-    button.addEventListener('click', () => calculator.selectOperation(button.innerText))
-  );
-  document.querySelector('[data-equals]')?.addEventListener('click', () => calculator.getResult());
-  document.querySelector('[data-delete]')?.addEventListener('click', () => calculator.delete());
-  document.querySelector('[data-all-clear]')?.addEventListener('click', () => calculator.clear());
+// fix
+  document
+    .querySelectorAll("[data-number]")
+    .forEach((button) =>
+      button.addEventListener("click", () =>
+        calculator.addNumber(button.innerText)
+      )
+    );
+  document
+    .querySelectorAll("[data-operation]")
+    .forEach((button) =>
+      button.addEventListener("click", () =>
+        calculator.selectOperation(button.innerText)
+      )
+    );
+  document
+    .querySelector("[data-equals]")
+    ?.addEventListener("click", () => calculator.getResult());
+  document
+    .querySelector("[data-delete]")
+    ?.addEventListener("click", () => calculator.delete());
+  document
+    .querySelector("[data-all-clear]")
+    ?.addEventListener("click", () => calculator.clear());
 });
