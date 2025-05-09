8a29b5cf22470e1f243fae07bc0842cad07411ff
yromanchak <yurij.romanchak@gmail.com>
2025-05-09 15:16:23 +0300

    webpack-change

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,54 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-
+// test rebase
 
 module.exports = {
-  mode: 'development', 
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'), 
-    filename: 'js/bundle-[contenthash].js', 
-    clean: true
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/bundle-[contenthash].js",
+    clean: true,
   },
   devServer: {
-    static: path.join(__dirname, 'dist'),
+    static: path.join(__dirname, "dist"),
     compress: true,
     port: 3000,
-    open: true
+    open: true,
   },
-  
+
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
+          loader: "babel-loader",
+        },
       },
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
       {
         test: /\.(png|jpe?g|webp|svg)$/,
-        type: 'asset/resource',
+        type: "asset/resource",
         generator: {
-          filename: 'assets/images/[name]-[hash][ext]'
-        }
-      }
-    ]
+          filename: "assets/images/[name]-[hash][ext]",
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-      scriptLoading: 'module'
+      template: "./src/index.html",
+      filename: "index.html",
+      scriptLoading: "module",
     }),
     new MiniCssExtractPlugin({
-      filename: 'css/styles-[contenthash].css'
-    })
-  ]
+      filename: "css/styles-[contenthash].css",
+    }),
+  ],
 };
